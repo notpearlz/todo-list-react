a0f56114d10b85a14174b70f6333088cd89b29c8
Bun <duypersonal512@gmail.com>
2025-02-27 03:37:41 -0600

    setup routing

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,27 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from 'react-router-dom'
 import './index.css'
-import App from './App.jsx'
+
+import App from './App'
+import Todo from './pages/todo-list'
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App/>,
+  },
+  {
+    path: "/todo",
+    element: <Todo/>
+  }
+]);
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </StrictMode>,
 )
---
+++ src/pages/todo-list.jsx
@@ -0,0 +1,10 @@
+import './todo-list.css'
+
+function TodoList(){
+    return (
+
+        <h1>Todo list page</h1>
+    );
+}
+
+export default TodoList;
