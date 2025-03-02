d078758d3cd0a62ef29c96aeadc91cdf86ca4db3
Bun <lolmebunbun@gmail.com>
2025-03-01 22:30:52 -0600

    test

--- src/pages/todo-list.jsx
+++ src/pages/todo-list.jsx
@@ -1,9 +1,50 @@
+import { useForm } from 'react-hook-form'
+import { useState } from 'react'
 import './todo-list.css'
 
 function TodoList(){
-    return (
+    // stores tasks persistently using zustand
 
-        <h1>Todo list page</h1>
+    const tasks = [];
+    
+
+
+    // create form using react-hook-form
+    const form = useForm()
+    const { register, handleSubmit, reset } = form
+
+
+    const onSubmit = (data) => {
+        tasks.push(data.task)
+        console.log(tasks);
+        reset();
+    }
+
+    return (
+        <>
+
+            <main>
+                <div className='todo-list'>
+                    <div>
+                        <h1>Todo List</h1>
+                        <form id="task-form" onSubmit={handleSubmit(onSubmit)}>
+                            <input 
+                            placeholder='add new task' 
+                            id="task" 
+                            {...register("task")}></input>
+                        </form>
+                    </div>
+
+                    <ul>
+                        <li>Test1 <button>X</button></li>
+
+
+                    </ul>
+
+                </div>
+
+            </main>
+        </>
     );
 }
 
